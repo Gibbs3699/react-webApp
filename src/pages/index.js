ed0d2080461e8e222bff7a4390f46b54e7a0a8ff
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-02-07 16:19:03 +0700

    Adding Basic Styling with CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Dessadasdsdsadasdas</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>React Template</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">WATCH THE VIDEO</Link>
+      </div>
+    </div>
   </div>
 )
 
