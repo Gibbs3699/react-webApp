02d7b1b7a22396e5e0296ea4911e6f8c0c49bac3
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-02-08 22:48:31 +0700

    Adding States and Events

--- src/components/header.js
+++ src/components/header.js
@@ -2,17 +2,42 @@ import React from 'react'
 import Link from 'gatsby-link'
 // import logo from '../images/logo-designcode.svg' //can used require as in line 8
 import './Header.css'
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className='HeaderGroup'>
-      <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30"/></Link>
-      <Link to="/course">Course</Link>
-      <Link to="/downloads">Downloads</Link>
-      <Link to="/workshops">Workshops</Link>
-      <Link to="/buy"><button>Buy</button></Link>
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.hasScrolled)
+  }
+
+  hasScrolled = (event) => {
+    const scrollTap = window.pageYOffset
+
+    if (scrollTap > 50) {
+      this.setState({ hasScrolled: true})
+    }else {
+      this.setState({ hasScrolled: false})
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+      <div className='HeaderGroup'>
+        <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30"/></Link>
+        <Link to="/course">Course</Link>
+        <Link to="/downloads">Downloads</Link>
+        <Link to="/workshops">Workshops</Link>
+        <Link to="/buy"><button>Buy</button></Link>
+      </div>
     </div>
-  </div>
-)
+    )
+  }
+}
 
 export default Header
